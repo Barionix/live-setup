6462f5f274024c3415ba9c166b0e0f37d8ba0f4b
Live session user <mariojuniorsoaresdev@gmail.com>
2021-07-26 23:00:02 +0000

    add git cloning

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import os
 
-def set_folder():
-    print("[Setting folders]")
-    os.system("cd; mkdir Officer")
+def clone_repos():
+    print("[Cloning repos...]")
+    os.system("git clone https://github.com/Barionix/Trovilo")
+    os.system("git clone https://github.com/Barionix/gogram")
+    os.system("git clone https://github.com/Barionix/live-setup")
+
+def set_folders():
+    print("[Setting folders...]")
+    os.system("cd; mkdir Office")
+    clone_repos()
 
 def install_dev_tools():
-    print("[Installing git]")
+    print("[Installing git...]")
     os.system("sudo apt-get install -y git")
-    print("[Installing VSCode]")
+    print("[Installing VSCode...]")
     os.system("sudo snap install --classic code"    
 def start():
-    set_folder()
+    set_folders()
     install_dev_tools()
 
 start()
